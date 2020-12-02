8a43a09128f7f755a4de64b5c1bfa1c1b352d8c1
Jjh <jpsfebk@gmail.com>
2020-12-02 10:13:15 +0800

    準備架設API，從Service開始將資料結合並遞給API

---
+++ Response_Test/Response_Test/Controllers/API/TestOneController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Response_Test.Controllers.API
+{
+    [RoutePrefix("api/[Controller]/[action]")]
+    public class TestOneController : ApiController
+    {
+        [HttpGet]
+        public List<string> TestOne()
+        {
+            List<string> result = new List<string>();
+            return result;
+        }
+    }
+}
---
+++ Response_Test/Response_Test/Service/DataforTestOne.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Response_Test.Helper;
+
+namespace Response_Test.Service
+{
+    public class DataforTestOne
+    {
+    }
+}
